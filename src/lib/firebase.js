221c7a835abf528d534c7f92588e71c1d64351d5
PDinesh0103 <dineshd0987@gmail.com>
2026-01-13 13:56:10 +0530

    API KEY SECURED INITIALISATION

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -8,14 +8,13 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCjG51UL-iCEWa3fSfasq4YPzqEMWPmDUw",
-  authDomain: "xplore-18a85.firebaseapp.com",
-  databaseURL: "https://xplore-18a85-default-rtdb.firebaseio.com",
-  projectId: "xplore-18a85",
-  storageBucket: "xplore-18a85.firebasestorage.app",
-  messagingSenderId: "467214770165",
-  appId: "1:467214770165:web:01b4acfa11d61469a20abc",
-  measurementId: "G-DGFTNJZWRG"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
