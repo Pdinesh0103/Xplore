20682f3c7e31d3c7935df26a96ff47d096ee7520
PDinesh0103 <dineshd0987@gmail.com>
2026-01-13 18:09:59 +0530

    Devlopment version

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,14 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig: NextConfig = {
   output: 'export',
   images: {
     unoptimized: true,
   },
-  basePath: "/Xplore",
-  assetPrefix: "/Xplore/",
+  basePath: isProd ? "/Xplore" : undefined,
+  assetPrefix: isProd ? "/Xplore/" : undefined,
 };
 
 export default nextConfig;
